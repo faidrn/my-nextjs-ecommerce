5bc80a5600f32e780ac8fe793a351c7eae62f2ba
Fredy Izquierdo <fredyizquierdo.dev@gmail.com>
2026-01-07 22:29:01 +0100

    Add some styles

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,13 +6,13 @@ export default function HomePage() {
   return (
     <div>
       <header
-        className="sticky top-0 z-50 w-full border-b bg-white/95 backdrop-blur supports-backdrop-filter:bg-white/60 dark:bg-gray-950/95 dark:supports-backdrop-filter:bg-gray-950/60"
+        className="sticky top-0 z-50 w-full border-b border-b-gray-300 bg-white/95 backdrop-blur supports-backdrop-filter:bg-white/60 dark:bg-gray-950/95 dark:supports-backdrop-filter:bg-gray-950/60"
       >
         <Navbar />
       </header>
-      <main>
+      {/*<main>
         <ProductGrid />
-      </main>
+      </main>*/}
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,69 @@
 import Link from "next/link";
-import { ShoppingCart } from "lucide-react";
+import { ShoppingCart, Moon, Sun, Globe } from "lucide-react";
 
 
 const Navbar = () => {
   return (
     <nav
-      className=""
+      className="flex items-center justify-between gap-6 px-4 py-4 md:px-8"
     >
-      <Link href="/">
-        <div>
-          <ShoppingCart size={30} />
+      <Link 
+        href="/"
+        className="flex items-center space-x-2 transition-opacity hover:opacity-80"
+      >
+        <div
+          className="flex h-8 w-8 items-center justify-center rounded-lg bg-linear-to-br from-blue-600 to-purple-600"
+        >
+          <ShoppingCart 
+            className="h-5 w-5 text-white"
+          />
         </div>
-        Shophub
+        <span 
+          className="text-xl font-semibold dark:text-white"
+        >
+          ShopHub
+        </span>
       </Link>
 
-      <ul>
+      <ul
+        className="flex justify-center items-center gap-4"
+      >
         <li>
-          <Link href="/products">Lenguajes</Link>
+          <Link href="/products">
+            <Globe 
+              className="h-5 w-5"
+            />
+            <div
+              className="bg-blue-600 text-white font-bold rounded-2xl px-1 text-[11px] items-center justify-center flex absolute left-2 top-3"
+            >
+              <span
+              >
+                EN
+              </span>
+              {/*<span>ES</span>*/}
+            </div>
+          </Link>
         </li>
         <li>
-          <Link href="/products">Tema</Link>
+          <Link href="/products">
+            <Sun 
+              className="h-5 w-5"
+            />
+          </Link>
         </li>
         <li>
-          <Link href="/products">Carrito</Link>
+          <Link href="/products">
+            <Moon 
+              className="h-5 w-5"
+            />
+          </Link>
+        </li>
+        <li>
+          <Link href="/products">
+            <ShoppingCart 
+              className="h-5 w-5"
+            />
+          </Link>
         </li>
       </ul>
     </nav>
